754c24b7f5e39d61a43319caf49d36f22ede13ef
joepurin2000 <joepurin2000@gmail.com>
2022-11-06 15:29:17 +0700

    send work

---
+++ HealthcareWorkerTeam.java
@@ -0,0 +1,40 @@
+package edu.parinya.softarchdesign.structural;
+
+import java.util.*;
+
+public class HealthcareWorkerTeam implements HealthcareServiceable{
+    private Set<HealthcareServiceable> members = new HashSet<HealthcareServiceable>();
+
+    public HealthcareWorkerTeam(){}
+
+    public void addMember(HealthcareServiceable healthcareServiceable){
+        this.members.add(healthcareServiceable);
+    }
+
+    public void removeMember(HealthcareServiceable healthcareServiceable){
+        this.members.remove(healthcareServiceable);
+    }
+
+    @Override
+    public void service(){
+        // for (HealthcareServiceable healthcareServiceable: this.members) {
+        //     healthcareServiceable.service();
+        // }
+        Iterator<HealthcareServiceable> value = members.iterator();
+        while(value.hasNext()){
+            
+            value.next().service();
+        }
+    }
+
+    @Override
+    public double getPrice(){
+        double sum = 0;
+
+        for (HealthcareServiceable healthcareServiceable: this.members) {
+            sum += healthcareServiceable.getPrice();
+        }
+
+        return sum;
+    }
+}
